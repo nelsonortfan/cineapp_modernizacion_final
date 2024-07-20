227132f34c3d7ff0c3df19f7d9847d45a7da223d
Nelson <n.ortizf@uniandes.edu.co>
2024-07-20 17:57:44 -0500

    Uso de lombok para backend

--- Backend/cineapp/src/main/java/net/itinajero/app/cineapp/model/Detalle.java
+++ Backend/cineapp/src/main/java/net/itinajero/app/cineapp/model/Detalle.java
@@ -8,9 +8,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "Detalles")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Detalle {
 
 	@Id
@@ -20,58 +28,5 @@ public class Detalle {
 	private String actores;
 	private String sinopsis;
 	private String trailer; // url del video de YouTube
-
-	/**
-	 * Constructor sin parametros
-	 */
-	public Detalle() {
-
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getDirector() {
-		return director;
-	}
-
-	public void setDirector(String director) {
-		this.director = director;
-	}
-
-	public String getActores() {
-		return actores;
-	}
-
-	public void setActores(String actores) {
-		this.actores = actores;
-	}
-
-	public String getSinopsis() {
-		return sinopsis;
-	}
-
-	public void setSinopsis(String sinopsis) {
-		this.sinopsis = sinopsis;
-	}
-
-	public String getTrailer() {
-		return trailer;
-	}
-
-	public void setTrailer(String trailer) {
-		this.trailer = trailer;
-	}
-
-	@Override
-	public String toString() {
-		return "Detalle [id=" + id + ", director=" + director + ", actores=" + actores + ", sinopsis=" + sinopsis
-				+ ", trailer=" + trailer + "]";
-	}
 
 }

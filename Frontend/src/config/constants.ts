0502b6938142bd779085cb156b1e1b87e2e9cb1b
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2024-07-26 04:26:57 -0500

    leave constansts as it was

--- Frontend/src/config/constants.ts
+++ Frontend/src/config/constants.ts
@@ -1,6 +1,6 @@
-// # Local deploy
+# Local deploy
 export const GET_MOVIES_URL = "http://localhost:8080/peliculas/index";
 export const CREATE_MOVIE_URL = "http://localhost:8080/peliculas/save";
-// # Prod deploy
-// export const GET_MOVIES_URL = "http://54.162.231.100:8080/peliculas/index";
-// export const CREATE_MOVIE_URL = "http://54.162.231.100:8080/peliculas/save";
+# Prod deploy
+export const GET_MOVIES_URL = "http://54.162.231.100:8080/peliculas/index";
+export const CREATE_MOVIE_URL = "http://54.162.231.100:8080/peliculas/save";
